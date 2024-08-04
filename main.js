261e9cecfb0d0f3b709cf6282940ac32ccc8a881
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2024-08-03 23:54:20 -0300

    finaluno

--- main.js
+++ main.js
@@ -77,3 +77,16 @@ function diaEventoAhora () {
         
     })
   });
+  
+  function exito(){
+    Swal.fire({
+        title: '<strong>VER LA UBICACION<u></u></strong>',
+        icon: 'success',
+        html:
+          'INGRESÁ' +
+          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3289.577080336622!2d-58.64133612489268!3d-34.46288205007295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca58f16d59351%3A0x5c22a695fe3d1e1f!2sSmile%20sal%C3%B3n%20de%20fiestas!5e0!3m2!1ses-419!2sar!4v1722641278724!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+          'Podes ampliar el mapa',
+          
+          
+      })
+}
